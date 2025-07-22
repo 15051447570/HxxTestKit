37a530c1cc27c98f8b5a8a850bd0adfa785d17d0
15051447570@163.com <15051447570@163.com>
2025-07-22 22:45:36 +0800

    first commit

---
+++ HHTestKit/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  HHTestKit
+//
+//  Created by 周辉 on 2025/7/22.
+//
+
+import Foundation
+open class HHLogger : NSObject {
+    
+    public static func testLog() {
+        print("public static 啦啦啦啦======")
+    }
+    
+    public func sayHelloWorld() {
+        print("public static sayHelloWorld=======")
+    }
+}
