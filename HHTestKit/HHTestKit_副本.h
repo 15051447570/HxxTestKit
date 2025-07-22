73ecc02708e6548a94ed9dc035022ccb7ff70120
15051447570@163.com <15051447570@163.com>
2025-07-22 23:20:15 +0800

    0000d

---
+++ HHTestKit/HHTestKit_副本.h
@@ -0,0 +1,18 @@
+//
+//  HHTestKit.h
+//  HHTestKit
+//
+//  Created by 周辉 on 2025/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HHTestKit.
+FOUNDATION_EXPORT double HHTestKitVersionNumber;
+
+//! Project version string for HHTestKit.
+FOUNDATION_EXPORT const unsigned char HHTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHTestKit/PublicHeader.h>
+
+
